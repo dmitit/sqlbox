a808a5a82537f5b1f9143d3a77a18fc2dceee124
dmitrevnik <dmitrevnik@gmail.com>
2025-06-09 18:54:55 +0300

    Remove bundle analyzer

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
-import analyzer from 'vite-bundle-analyzer';
+// import analyzer from 'vite-bundle-analyzer';
 
 const root = path.resolve(__dirname, 'src');
 
 // https://vite.dev/config/
 export default defineConfig({
    mode: 'development',
-   plugins: [react(), tailwindcss(), analyzer({ analyzerMode: 'server' })],
+   plugins: [react(), tailwindcss()],
    resolve: {
       alias: {
          '@': root,
